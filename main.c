9ba845cde2eb1e513b8afefdc6c154d7473c901b
jt7williams <43657435+jt7williams@users.noreply.github.com>
2019-12-10 17:01:38 -0800

    update main and added additional files.

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include <avr/delay.h>
 #include <pwmodulation.h>
 #include "i2c.h"
-#include "io.h"
 
 #define F_CPU 8000000UL		/* Define CPU Frequency e.g. here its 8MHz */
 #include <avr/io.h>		/* Include AVR std. library file */
@@ -13,15 +12,23 @@
 #include <math.h>
 #include "timer2.h"
 
-#define M1 3
+
+
+#define M1 1
 #define M2 4
-#define M3 5
-#define M4 6
-unsigned char M1_Throtle = 11;
-unsigned char M2_Throtle = 11;
-unsigned char M3_Throtle = 11;
-unsigned char M4_Throtle = 11;
-unsigned char Sensitity = 5;
+#define M3 0
+#define M4 3
+unsigned char PortD_Status = 0;
+double	M1_Throtle = 8000;
+double  M2_Throtle = 8000;
+double	M3_Throtle = 8000;
+double	M4_Throtle = 8000;
+double  Max_trotle = 14500;
+unsigned char Emergency_Stop = 0;
+
+unsigned char Sensitity = 1;
+unsigned char adjustment_speed = 1;
+unsigned char adjustment_speed1 = 5;
 int x_angle = 0;
 int y_angle = 0;
 
@@ -53,9 +60,22 @@ unsigned char data[10] = {'x',':',' ','+','0','0','0','0','0','\0'};
 
 	
 
-
+void SetModulation(short t)
+{
+	OCR1A = ICR1 - t; //18000
+}
 #include <avr/io.h>
-void move_servo(unsigned char  x, unsigned char servo_pin ){
+void pwm_init(short t)
+{
+	TCCR1A |= 1<<WGM11 | 1<<COM1A1 | 1<<COM1A0;
+	TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS10;
+	ICR1 = 2999;
+	OCR1A = ICR1 - t;
+	//RANGE 850 -
+	//SetModulation(t);
+
+}
+void move_servo(double  x, unsigned char servo_pin ){
 	unsigned char s_value;
 	switch(servo_pin){
 		case 0:
@@ -84,17 +104,19 @@ void move_servo(unsigned char  x, unsigned char servo_pin ){
 		break;
 	}
 	PORTD = s_value;;
-	_delay_ms(x);
+	_delay_us(x);
 	PORTD = 0x00;
-	_delay_ms(20-x);
+	_delay_us(20000-x);
 	
 }
-void move_servo_calibrate(unsigned char  x){
-	PORTD = 0xF8;;
-	_delay_ms(x);
-	PORTD = 0x00;
-	_delay_ms(20-x);
-	
+void move_servo_calibrate(double x){
+
+		
+		PORTD = 0xFF;
+		_delay_ms(x);
+		PORTD = 0x00;
+		_delay_ms(20-x);
+		
 }
 void findnum(int t){
 	 int temp = t;
@@ -119,17 +141,18 @@ void findnum(int t){
 
 
 void CalibrateESC()
-{
-	for(unsigned char i = 5; i < 8; i++){
-			PORTC = 0x00;
-			_delay_ms(100);
-			PORTC = 0xFF;
+{ 
+	
+	for(int i = 5; i < 8; i++ ){
 			while(!TimerFlag){
 				move_servo_calibrate(i);
 			}
 			TimerFlag = 0;
 			
+
+			
 		}
+
 }
 
 void GyroAcel_init()
@@ -165,7 +188,7 @@ short GyroAcel_read(uint8_t registers)
 		i2cStart(0x69);
 		i2cSend(registers);
 		i2cStop();
-		_delay_ms(1);
+		_delay_us(50); //DO NOT CHANGE THIS
 		i2cStartRead(0x69);
 		char dta1 =  i2cReadAck();
 		char dta2 = i2cReadNoAck();
@@ -176,22 +199,7 @@ short GyroAcel_read(uint8_t registers)
 	//findnum(variable);
 }
 
-void SetModulation(short t)
-{
-	OCR1A = ICR1 - t; //18000
-}
 
-void pwm_init(short t)
-{
-DDRD |= 0xFF;
-TCCR1A |= 1<<WGM11 | 1<<COM1A1 | 1<<COM1A0;
-TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS10;
-ICR1 = 2999;
-
-//RANGE 850 -
-SetModulation(t);
-
-}
 void Update_Angles()
 {
 	Gyr_rawX = GyroAcel_read(0x0C);
@@ -207,153 +215,194 @@ void Update_Angles()
 			    
 }
 
- int main(void)
- {
-	DDRD = 0xFF;
-	DDRA = 0x00;
-// 	DDRC = 0xFF;
-// 	
-	TimerSet(5000);
-	TimerOn();
-// 	
-	CalibrateESC(); //TimerSet must be 4500ms and On before calling this function.*/ 
-
-// 
-// 					
-// 
-// 		
-// 
-// 		unsigned char a;
-// 		TimerSet(10);
-// 			short temp = 810;
-// 		pwm_init(temp);
-// 	while(1){
-// 		
-// 		SetModulation(temp);
-// // 		_delay_ms(1);
-// // 		a = ~PINA & 0x01;
-// // 		a = a << 3;
-// // 		PORTD  = a;
-// 		while(!TimerFlag){
-// 			
-// 		}
-// 		TimerFlag = 0;
-// 		temp++;
-// 		if(temp == 950){
-// 			temp = 810;
-// 		}
-// 		
-// 		
-// 	}
-
-		
-
-
-		
-		//PORTD = a << 6;
-
-		
-		
-		
-		
-			
-
-			
-//	}
-	
+void TiltTowardsNegativeY()
+{
+	if((Acceleration_angle[1] < y_angle - Sensitity)){
 	
 
-
-	
-	//DDRD = 0xFF;
-	
-	
-	//Initialize gyro
-	GyroAcel_init();
-
-	
-	
-	
-	//Read gyro:
-	
-	//unsigned char j = 1;
-	
-	
-		x_angle = Acceleration_angle[0] = (atan((Acc_rawY/16384.0)/sqrt(pow((Acc_rawX/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg);
-		y_angle = Acceleration_angle[1] = atan(-1*(Acc_rawX/16384.0)/sqrt(pow((Acc_rawY/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
-	
-	while(1)
-	{
-		
-		//PORTD = PORTD | 0x80;
-					_delay_ms(100);
-		
-					move_servo(M1_Throtle,M1);
- 					move_servo(M4_Throtle,M4);
- 					move_servo(M3_Throtle,M3);
-  					move_servo(M2_Throtle,M2);
-		
-		
-		//A TILT TOWARDS -Y WHEN X IS AT BALANCE(-10<X<10)
-		if((Acceleration_angle[1] < y_angle - Sensitity) && ((Acceleration_angle[0] > - Sensitity) && (Acceleration_angle[0] < Sensitity) )){
-			//_delay_ms(10);
-			while((Acceleration_angle[1] < y_angle - Sensitity) && ((Acceleration_angle[0] > - Sensitity) && (Acceleration_angle[0] < Sensitity) )){
-					move_servo(M1_Throtle,M1);
-		 			//move_servo(M4_Throtle-1,M4);
-		   			move_servo(M3_Throtle,M3);
-					//move_servo(M2_Throtle-1,M2);
-					Update_Angles();
-			}
-
+		if(M1_Throtle > 8000){
+			M1_Throtle -= adjustment_speed;
 		}
-		//A TILT TOWARDS Y WHEN X IS AT BALANCE(-10<X<10)
-		if((Acceleration_angle[1] > y_angle + Sensitity) && ((Acceleration_angle[0] > - Sensitity) && (Acceleration_angle[0] <  Sensitity))){
-			//_delay_ms(20);
-			while((Acceleration_angle[1] > y_angle + Sensitity) && ((Acceleration_angle[0] > - Sensitity) && (Acceleration_angle[0] < Sensitity))){
-				move_servo(M4_Throtle,M4);
-				move_servo(M2_Throtle,M2);
-				Update_Angles();
-			}
+		if(M3_Throtle > 8000){
+			M3_Throtle -= adjustment_speed;
 		}
-		if((Acceleration_angle[0] < x_angle - Sensitity) && ((Acceleration_angle[1] > -Sensitity) && (Acceleration_angle[1] < Sensitity) )){
-			while((Acceleration_angle[0] < x_angle - Sensitity) && ((Acceleration_angle[1] > -Sensitity) && (Acceleration_angle[1] < Sensitity) )){
-				move_servo(M1_Throtle,M1);
-
-				move_servo(M2_Throtle,M2);
-				Update_Angles();
-			
-			}
-		}
-		if((Acceleration_angle[0] > x_angle + Sensitity) && ((Acceleration_angle[1] > -Sensitity) && (Acceleration_angle[1] < Sensitity) )){
-			_delay_ms(50);
-			while((Acceleration_angle[0] > x_angle + Sensitity) && ((Acceleration_angle[1] > -Sensitity) && (Acceleration_angle[1] < Sensitity) )){
-				move_servo(M4_Throtle,M4);
-				move_servo(M3_Throtle,M3);
-				Update_Angles();
-			}
-
-		}
-
-// 		if(Acceleration_angle[1] > y_angle +10){
-// 			_delay_ms(100);
-// 			move_servo(M1_Throtle,M1);
-// 			move_servo(M4_Throtle+1,M4);
-// 			
-// 		}
-		//else(y_angle > Acceleration_angle[1])
 		
-	
-
-		//Read Gyro:
-		// ReadValues
-			Update_Angles();
-
-
-
+		//increase weak motor
+		if(M2_Throtle < Max_trotle){
+			M2_Throtle += adjustment_speed1;
+		}
+		if (M4_Throtle < Max_trotle)
+		{
+			M4_Throtle += adjustment_speed1;
+		}
 		
 
 
 	}
 }
+
+void TiltTowardsPositiveY()
+{
+	if((Acceleration_angle[1] > y_angle + Sensitity) ){
+		if(M2_Throtle > 8000){
+			M2_Throtle -= adjustment_speed;
+		}
+		if (M4_Throtle > 8000)
+		{
+			M4_Throtle -= adjustment_speed;
+		}
+		
+
+		//increase weak motor
+		if(M3_Throtle < Max_trotle){
+			M3_Throtle += adjustment_speed1;
+		}
+		if (M1_Throtle < Max_trotle)
+		{
+			M1_Throtle += adjustment_speed1;
+		}
+		
+	}
+}
+
+void TiltTowardsNegativeX()
+{
+	if((Acceleration_angle[0] < x_angle - Sensitity) ){
+		
+		
+		if(M2_Throtle > 8000){
+			M2_Throtle -= adjustment_speed;
+		}
+		if(M1_Throtle > 8000){
+			M1_Throtle -= adjustment_speed;
+		}
+		
+		//increase weak motors
+		if(M3_Throtle < Max_trotle ){
+			M3_Throtle += adjustment_speed1;
+		}
+		if(M4_Throtle < Max_trotle){
+			M4_Throtle += adjustment_speed1;
+		}
+
+	 		}
+}
+
+void TiltTowardsPositiveX()
+{
+	if((Acceleration_angle[0] > x_angle + Sensitity) ){
+
+		if(M3_Throtle > 8000){
+			M3_Throtle -= adjustment_speed;
+		}
+		if(M4_Throtle > 8000){
+			M4_Throtle -= adjustment_speed;
+		}
+		
+		
+		//increase weak motor
+		if(M2_Throtle < Max_trotle){
+			M2_Throtle += adjustment_speed1;
+		}
+		if (M1_Throtle < Max_trotle)
+		{
+			M1_Throtle += adjustment_speed1;
+		}
+	}
+}
+
+ int main(void)
+ {
+	DDRD = 0xFF; 
+	DDRC = 0xFF ;PORTC = 0xFF; 
+	DDRA = 0xF0; PORTA = 0x0F;;
+	DDRB = 0x00; PORTB = 0xFF;
+
+	TimerSet(4900);
+	TimerOn();
+	CalibrateESC();
+
+		
+	
+	//Initialize gyro
+	GyroAcel_init();
+	TimerSet(25);
+	Update_Angles();
+	
+	
+	y_angle = 9;
+	x_angle = 5;
+	
+	
+	
+	
+		x_angle = Acceleration_angle[0] = (atan((Acc_rawY/16384.0)/sqrt(pow((Acc_rawX/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg);
+		y_angle = Acceleration_angle[1] = atan(-1*(Acc_rawX/16384.0)/sqrt(pow((Acc_rawY/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
+	int i = 0;
+	while(1)
+	{
+		while(!TimerFlag){
+								
+								move_servo(M1_Throtle,M1);
+								move_servo(M4_Throtle,M4);
+								move_servo(M3_Throtle,M3);
+								move_servo(M2_Throtle,M2);
+		}
+		TimerFlag = 0;
+		
+		//PORTD = PORTD | 0x80;
+		if(i < 1000){
+			M1_Throtle +=6;
+			M2_Throtle+=6;
+			M3_Throtle+=6;
+			M4_Throtle+=6;
+			i++;
+		}
+		else{
+			M1_Throtle = M2_Throtle = M3_Throtle = M4_Throtle = 8000;
+		}
+		
+		
+		
+		//A TILT TOWARDS -Y WHEN Y is not within desired degree(y_angle)
+		TiltTowardsNegativeY();
+
+		//A TILT TOWARDS Y WHEN Y is not within desired degree(y_angle)
+		TiltTowardsPositiveY();
+
+		//A TILT TOWARDS -X WHEN X is not within desired degree(x_angle)
+		TiltTowardsNegativeX();
+		
+		//A Tilt TOWARDS X WHEN X is not within desired degree(x_angle)
+		TiltTowardsPositiveX();
+
+			
+
+	
+
+		Update_Angles();
+	if(bInput < 15 && bInput > 0) {
+		if(bInput == 0x01) {
+			x_angle+=5;
+		} else if(bInput == 0x02) {
+			x_angle-=5;
+		} else if(bInput == 0x04) {
+			y_angle-=5;
+		} else if(bInput == 0x08) {
+			y_angle+=5;
+		}
+	} else {
+		x_angle = 0;
+		y_angle = 0;
+	}
+
+	}
+
+
+	
+
+}
+
+
 
 
